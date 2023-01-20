9d851ded225071c6ee86e13d8c8e735f17dc4914
Liam <liam@scalzulli.com>
2023-01-20 00:03:11 -0500

    Run format

--- src/A1.ml
+++ src/A1.ml
@@ -1,46 +1,33 @@
 (* Question 1: Manhattan Distance *)
 
-let distance_tests = [
-  (((2, 2), (2, 2)), 0);
-  (((20, 30), (10, 20)), 20);
-  (((10, 20), (20, 30)), 20);
-  (((1, 1), (3, 2)), 3);
-  (((-3, 2), (-50, 1)), 48);
-]
+let distance_tests =
+  [ (((2, 2), (2, 2)), 0)
+  ; (((20, 30), (10, 20)), 20)
+  ; (((10, 20), (20, 30)), 20)
+  ; (((1, 1), (3, 2)), 3)
+  ; (((-3, 2), (-50, 1)), 48) ]
 
-let distance (x1, y1) (x2, y2) =
-  abs (x2 - x1) + abs (y2 - y1)
+let distance (x1, y1) (x2, y2) = abs (x2 - x1) + abs (y2 - y1)
 
 (* Question 2: Binomial *)
 
-let binomial_tests = [
-  ((3, 2), 3);
-  ((20, 10), 11);
-  ((1, 1), 1);
-  ((0, 0), 1);
-  ((9, 2), 36);
-  ((1, 0), 1);
-]
+let binomial_tests =
+  [ ((3, 2), 3)
+  ; ((20, 10), 11)
+  ; ((1, 1), 1)
+  ; ((0, 0), 1)
+  ; ((9, 2), 36)
+  ; ((1, 0), 1) ]
 
 let binomial n k =
-  let rec factorial n =
-    if n <= 1 then 1 else n * factorial (n - 1)
-  in factorial n / (factorial k * factorial (n - k))
+  let rec factorial n = if n <= 1 then 1 else n * factorial (n - 1) in
+  factorial n / (factorial k * factorial (n - k))
 
 (* Question 3: Lucas Numbers *)
 
-let lucas_tests = [
-  ((0), 2);
-  ((1), 1);
-  ((3), 4);
-  ((10), 123);
-]
+let lucas_tests = [(0, 2); (1, 1); (3, 4); (10, 123)]
 
 let rec lucas_helper n a b =
-  match n with
-    0 -> a
-  | 1 -> b
-  | n -> lucas_helper (n - 1) b (a + b)
+  match n with 0 -> a | 1 -> b | n -> lucas_helper (n - 1) b (a + b)
 
-let lucas n =
-  lucas_helper n 2 1
+let lucas n = lucas_helper n 2 1
--- src/A2.ml
+++ src/A2.ml
@@ -1,89 +1,86 @@
 (* Question 1 *)
 
-let q1a_nat_of_int_tests : (int * nat) list = [
-  (0, Z);
-  (1, S Z);
-  (5, S (S (S (S (S Z)))))
-]
+let q1a_nat_of_int_tests : (int * nat) list =
+  [(0, Z); (1, S Z); (5, S (S (S (S (S Z)))))]
 
 let q1a_nat_of_int (n : int) : nat =
-  let rec aux n acc =
-    match n with
-    | 0 -> acc
-    | n -> aux (n - 1) (S acc)
-  in aux n Z
+  let rec aux n acc = match n with 0 -> acc | n -> aux (n - 1) (S acc) in
+  aux n Z
 
-let q1b_int_of_nat_tests : (nat * int) list = [
-  (Z, 0);
-  (S Z, 1);
-  (S (S (S (S (S Z)))), 5)
-]
+let q1b_int_of_nat_tests : (nat * int) list =
+  [(Z, 0); (S Z, 1); (S (S (S (S (S Z)))), 5)]
 
 let q1b_int_of_nat (n : nat) : int =
-  let rec aux n acc =
-    match n with
-    | Z -> acc
-    | S n -> aux n (acc + 1)
-  in aux n 0
+  let rec aux n acc = match n with Z -> acc | S n -> aux n (acc + 1) in
+  aux n 0
 
-let q1c_add_tests : ((nat * nat) * nat) list = [
-  ((Z, Z), Z);
-  ((Z, S Z), S Z);
-  ((S Z, Z), S Z);
-  ((S Z, S Z), S (S Z))
-]
+let q1c_add_tests : ((nat * nat) * nat) list =
+  [((Z, Z), Z); ((Z, S Z), S Z); ((S Z, Z), S Z); ((S Z, S Z), S (S Z))]
 
 let rec q1c_add (n : nat) (m : nat) : nat =
   match (n, m) with
-  | (Z, Z) -> m
-  | (Z, S _) -> m
-  | (S _, Z) -> n
-  | (S a, S _) -> q1c_add a (S m)
+  | Z, Z ->
+      m
+  | Z, S _ ->
+      m
+  | S _, Z ->
+      n
+  | S a, S _ ->
+      q1c_add a (S m)
 
 (* Question 2 *)
 
-let q2a_neg (e : exp) : exp =
-  Times (e, Const (-1.0))
+let q2a_neg (e : exp) : exp = Times (e, Const (-1.0))
 
-let q2b_minus (e1 : exp) (e2 : exp) : exp =
-  Plus (e1, q2a_neg e2)
+let q2b_minus (e1 : exp) (e2 : exp) : exp = Plus (e1, q2a_neg e2)
 
 let rec q2c_pow (e1 : exp) (p : nat) : exp =
-  match p with
-  | Z -> Const 1.0
-  | S n -> Times (e1, q2c_pow e1 n)
+  match p with Z -> Const 1.0 | S n -> Times (e1, q2c_pow e1 n)
 
 (* Question 3 *)
 
-let eval_tests : ((float * exp) * float) list = [
-  ((1.0, Const 2.0), 2.0);
-  ((1.0, Var), 1.0);
-  ((2.0, Plus(Var, Div(Const 2.0, Times(Var, Const 1.0)))), 3.0);
-  ((-2.0, Plus(Var, Div(Const 2.0, Times(Var, Const 1.0)))), -3.0)
-]
+let eval_tests : ((float * exp) * float) list =
+  [ ((1.0, Const 2.0), 2.0)
+  ; ((1.0, Var), 1.0)
+  ; ((2.0, Plus (Var, Div (Const 2.0, Times (Var, Const 1.0)))), 3.0)
+  ; ((-2.0, Plus (Var, Div (Const 2.0, Times (Var, Const 1.0)))), -3.0) ]
 
 let rec eval (a : float) (e : exp) : float =
   match e with
-  | Const x -> x
-  | Var -> a
-  | Plus (lhs, rhs) -> eval a lhs +. eval a rhs
-  | Times (lhs, rhs) -> eval a lhs *. eval a rhs
-  | Div (lhs, rhs) -> eval a lhs /. eval a rhs
+  | Const x ->
+      x
+  | Var ->
+      a
+  | Plus (lhs, rhs) ->
+      eval a lhs +. eval a rhs
+  | Times (lhs, rhs) ->
+      eval a lhs *. eval a rhs
+  | Div (lhs, rhs) ->
+      eval a lhs /. eval a rhs
 
 (* Question 4 *)
 
-let diff_tests : (exp * exp) list = [
-  (Const 1.0, Const 0.0);
-  (Var, Const 1.0);
-  (Plus(Var, Var), Plus (Const 1.0, Const 1.0));
-  (Times(Var, Var), Plus (Times (Const 1.0, Var), Times (Var, Const 1.0)));
-  (Div(Var, Var), Div (Plus (Times (Const 1.0, Var), q2a_neg (Times (Var, Const 1.0))), Times (Var, Var)));
-]
+let diff_tests : (exp * exp) list =
+  [ (Const 1.0, Const 0.0)
+  ; (Var, Const 1.0)
+  ; (Plus (Var, Var), Plus (Const 1.0, Const 1.0))
+  ; (Times (Var, Var), Plus (Times (Const 1.0, Var), Times (Var, Const 1.0)))
+  ; ( Div (Var, Var)
+    , Div
+        ( Plus (Times (Const 1.0, Var), q2a_neg (Times (Var, Const 1.0)))
+        , Times (Var, Var) ) ) ]
 
 let rec diff (e : exp) : exp =
   match e with
-  | Const _ -> Const 0.0
-  | Var -> Const 1.0
-  | Plus (lhs, rhs) -> Plus (diff lhs, diff rhs)
-  | Times (lhs, rhs) -> Plus (Times (diff lhs, rhs), Times (lhs, diff rhs))
-  | Div (lhs, rhs) -> Div (Plus (Times (diff lhs, rhs), q2a_neg (Times (lhs, diff rhs))), Times (rhs, rhs))
+  | Const _ ->
+      Const 0.0
+  | Var ->
+      Const 1.0
+  | Plus (lhs, rhs) ->
+      Plus (diff lhs, diff rhs)
+  | Times (lhs, rhs) ->
+      Plus (Times (diff lhs, rhs), Times (lhs, diff rhs))
+  | Div (lhs, rhs) ->
+      Div
+        ( Plus (Times (diff lhs, rhs), q2a_neg (Times (lhs, diff rhs)))
+        , Times (rhs, rhs) )

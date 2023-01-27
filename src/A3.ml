475c68948b731cb56991b36e776af7f4ee274ed3
Liam <liam@scalzulli.com>
2023-01-27 15:16:09 -0500

    [A3] Fix style errors

--- src/A3.ml
+++ src/A3.ml
@@ -1,9 +1,7 @@
 (* Question 1a *)
 
 let to_int_tests : (int church * int) list =
-  [ ((fun _ z -> z), 0)
-  ; ((fun s z -> s z), 1)
-  ; ((fun s z -> s (s (s (s (s z))))), 5) ]
+  [((fun _ z -> z), 0); ((fun s -> s), 1); ((fun s z -> s (s (s (s (s z))))), 5)]
 
 let to_int (n : int church) : int = n (fun x -> x + 1) 0
 
@@ -11,8 +9,8 @@ let to_int (n : int church) : int = n (fun x -> x + 1) 0
 
 let add_tests : (('b church * 'b church) * 'b church) list =
   [ (((fun _ z -> z), fun _ z -> z), fun _ z -> z)
-  ; (((fun s z -> s z), fun _ z -> z), fun s z -> s z)
-  ; (((fun _ z -> z), fun s z -> s z), fun s z -> s z) ]
+  ; (((fun s -> s), fun _ z -> z), fun s -> s)
+  ; (((fun _ z -> z), fun s -> s), fun s -> s) ]
 
 let add (n1 : 'b church) (n2 : 'b church) : 'b church = fun s z -> n1 s (n2 s z)
 
@@ -20,27 +18,25 @@ let add (n1 : 'b church) (n2 : 'b church) : 'b church = fun s z -> n1 s (n2 s z)
 
 let mult_tests : (('b church * 'b church) * 'b church) list =
   [ (((fun _ z -> z), fun _ z -> z), fun _ z -> z)
-  ; (((fun s z -> s z), fun _ z -> z), fun _ z -> z)
-  ; (((fun _ z -> z), fun s z -> s z), fun _ z -> z)
+  ; (((fun s -> s), fun _ z -> z), fun _ z -> z)
+  ; (((fun _ z -> z), fun s -> s), fun _ z -> z)
   ; ( ((fun s z -> s (s z)), fun s z -> s (s (s z)))
     , fun s z -> s (s (s (s (s (s z))))) ) ]
 
-let mult (n1 : 'b church) (n2 : 'b church) : 'b church = fun s z -> n1 (n2 s) z
+let mult (n1 : 'b church) (n2 : 'b church) : 'b church = fun s -> n1 (n2 s)
 
 (* Question 2a *)
 
 let is_even_tests : ('b church * bool) list =
-  [ ((fun _ z -> z), true)
-  ; ((fun s z -> s z), false)
-  ; ((fun s z -> s (s z)), true) ]
+  [((fun _ z -> z), true); ((fun s -> s), false); ((fun s z -> s (s z)), true)]
 
-let is_even (n : 'b church) : bool = n (fun x -> not x) true
+let is_even (n : 'b church) : bool = n not true
 
 (* Question 2b *)
 
 let gen_list_tests : (('b church * int) * int list) list =
   [ (((fun _ z -> z), 0), [])
-  ; (((fun s z -> s z), 0), [0])
+  ; (((fun s -> s), 0), [0])
   ; (((fun s z -> s (s z)), 0), [0; 0]) ]
 
 let gen_list (n : 'b church) (x : 'a) : 'a list = n (fun x' -> x :: x') []

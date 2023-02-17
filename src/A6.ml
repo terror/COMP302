3644df6430b4e610583635636331ba9a26f9167d
Liam <liam@scalzulli.com>
2023-02-17 16:46:02 -0500

    [A6] Fix style issues

--- src/A6.ml
+++ src/A6.ml
@@ -1,10 +1,10 @@
 (* Q1 : String to Characters to String *)
 
 let string_explode (s : string) : char list =
-  tabulate (fun i -> String.get s i) (String.length s)
+  tabulate (String.get s) (String.length s)
 
 let string_implode (l : char list) : string =
-  List.fold_right (fun a b -> a ^ b) (List.map (fun x -> String.make 1 x) l) ""
+  List.fold_right ( ^ ) (List.map (String.make 1) l) ""
 
 (* Q2 : Bank Account *)
 

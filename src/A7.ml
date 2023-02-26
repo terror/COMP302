641108efa6e17658dbfc3655101d282ccc14a19d
Liam <liam@scalzulli.com>
2023-02-25 21:35:10 -0500

    [A7] Add initial work

---
+++ src/A7.ml
@@ -0,0 +1,28 @@
+(* Question 1.1 *)
+
+let rec take (n : int) (s : 'a stream) : 'a list =
+  match n with 0 -> [] | n -> s.head :: take (n - 1) (force s.tail)
+
+(* Question 1.2 *)
+
+let rec lucas1 = int_stream_not_implemented
+
+(* Question 1.3 *)
+
+let rec unfold (f : 'a -> 'b * 'a) (seed : 'a) : 'b stream =
+  raise NotImplemented
+
+(* Question 1.3 *)
+
+let lucas : int stream = int_stream_not_implemented
+
+(* Question 2.1 *)
+
+let rec scale (s1 : int stream) (n : int) : int stream = raise NotImplemented
+
+let rec merge (s1 : 'a stream) (s2 : 'a stream) : 'a stream =
+  raise NotImplemented
+
+(* Question 2.2 *)
+
+let rec s = int_stream_not_implemented
